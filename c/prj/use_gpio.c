efc09151a69a32afffe4279808f9ef47a5984423
Work_desktop <creator83@yandex.ru>
2015-11-19 17:48:21 +0300

    19_11_15

--- c/prj/use_gpio.c
+++ c/prj/use_gpio.c
@@ -1,9 +1,22 @@
 #include "MK10D10.h"                    // Device header
 
+#define F_CPU 48000000
+#define tick 1000
+#define systick_ms 1
+#define systick_val (F_CPU/tick)*systick_ms
+
+
+void SysTick_Handler (void)
+{
+	
+}
+
+
 int main ()
 {
 	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;
 	PORTA->PCR [1] = PORT_PCR_MUX(1);
+	SysTick_Config (systick_val);
 	
 	while(1)
 	{
